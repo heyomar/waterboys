8ba4d8143bfcb46a5492a00c0e7b05bed5004d1e
Omar Rasool <orasool@hlkagency.com>
2016-04-15 17:19:24 -0500

    adds social links and images to footer

--- app/themes/waterboys/footer.php
+++ app/themes/waterboys/footer.php
@@ -15,9 +15,30 @@
 
   <footer role="contentinfo" class="site__footer">
     <div class="site__footer__content">
+        <div class="wb__footer__social wb__footer__flex-item">
+            <a href="https://twitter.com/waterboysorg"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/wb__home-footertwitter.svg" alt="" />@WaterboysORG</a>
+            <a href="https://www.facebook.com/waterboysorg"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/wb__home-footerfacebook.svg" alt="" />/WaterboysORG</a>
+            <a href="https://www.instagram.com/waterboysorg/"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/wb__home-footerinstagram.svg" alt="" />@WaterboysORG</a>
+        </div>
+        <nav class="wb__footer__links">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/wb__home-footerlogo.png" alt="" />
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/wb__home-chrislonglogo.png" alt="" />
+            <?php
+            $args = array(
+                'theme_location' => 'footer-bottom'
+            );
+            ?>
+            <?php wp_nav_menu($args); ?>
+        </nav>
+        <div class="wb__footer__nav wb__footer__flex-item">
+            <?php
+            $args = array(
+                'theme_location' => 'footer'
+            );
+             ?>
+            <?php wp_nav_menu($args); ?>
+        </div>
     </div>
   </footer>
-
-  <?php wp_footer(); ?>
 </body>
 </html>
