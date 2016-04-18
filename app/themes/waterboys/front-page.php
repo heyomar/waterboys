128b4025c2ef16e54359a2fc097eab1bc63d6907
Omar Rasool <orasool@hlkagency.com>
2016-04-18 16:51:18 -0500

    formats hero on homepage, adds mission link

--- app/themes/waterboys/front-page.php
+++ app/themes/waterboys/front-page.php
@@ -14,13 +14,14 @@
 <div class="wb__home-container">
 	<div class="wb__home-content">
     <h1 class="wb__hero-title">
-        Team Up to Tackle Thirst
+        Team Up to <br> Tackle Thirst
     <h2>
     <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/wb__home-wave.png" alt="" />
     <h3 class="wb__hero-subtitle">
         Waterboys unites players and fans from across the NFL in support of one mission:<br>bringing life-sustaining well water to East African communities in need.
     </h3>
-    <a href="" class="wb__button">Watch</a>
+    <a class="wb__button wb__hero-button" href="/" >Watch &nbsp; &#9654;</a><br>
+		<a class="wb__hero-missionlink" href="/">Our Mission</a>
 	</div>
 </div>
 
