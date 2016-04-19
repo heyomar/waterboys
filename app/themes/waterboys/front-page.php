130b9223291954fe21663814aad6d36b946e3d9a
Omar Rasool <orasool@hlkagency.com>
2016-04-19 16:49:58 -0500

    adds player name and team to header, styles, and tweaks php for template

--- app/themes/waterboys/front-page.php
+++ app/themes/waterboys/front-page.php
@@ -1,6 +1,5 @@
 <?php get_header(); ?>
 
-
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 		<div class="wb__home-container">
 			<div class="wb__home-content">
--- app/themes/waterboys/single-player.php
+++ app/themes/waterboys/single-player.php
@@ -8,24 +8,32 @@ Template Name: Player Profile
     $image = get_field('desktop_header');
     if( !empty($image) ): ?>
     <img class="wb__player__content__header-image" src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+    <div class="wb__player__header">
+        <h2 class="wb__player-name"><?php the_field('player_name'); ?></h2>
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/wb__home-wave.png" alt="" />
+        <p class="wb__player-team"><?php the_field('team'); ?></p>
+    </div>
   <?php endif; ?>
       <div class="wb__player-container">
-          <div class="wb__player-content wb__content">
-              <div class="wb__player__content-career"><p class="wb__player__content__title"><?php the_field('bio_title'); ?></p>
-              <p class="wb__player__content__body"><?php the_field('bio_body'); ?></p></div>
+          <div class="wb__player-content wb__content-bio">
+              <div class="wb__player__content-career">
+                <p class="wb__player__content__title wb__player__bio-title"><?php the_field('bio_title'); ?></p>
+                <p class="wb__player__content__body"><?php the_field('bio_body'); ?></p>
+              </div>
           </div>
           <div class="wb__player-content wb__content-info">
-            <p class="wb__player__content__info-position wb__player__content__title"><?php the_field('position_title'); ?></p>
+            <p class="wb__player__content__info-position wb__player__content__title wb__player__info-title"><?php the_field('position_title'); ?></p>
               <p><?php the_field('position'); ?></p>
-            <p class="wb__player__content__info-college wb__player__content__title"><?php the_field('college_title'); ?></p>
+            <p class="wb__player__content__info-college wb__player__content__title wb__player__info-title"><?php the_field('college_title'); ?></p>
               <p><?php the_field('college'); ?></p>
-            <p class="wb__player__content__info-careerhighlights wb__player__content__title"><?php the_field('careerhighlights_title'); ?></p>
+            <p class="wb__player__content__info-careerhighlights wb__player__content__title wb__player__info-title"><?php the_field('careerhighlights_title'); ?></p>
             <?php
             if( have_rows('career_highlights') ): ?>
                 <ul class="wb__player__career-ul">
                 <?php while ( have_rows('career_highlights') ) : the_row(); ?>
                     <li><?php the_sub_field('highlight');?></li>
                 <? endwhile;?>
+                </ul>
             <?php else : ?>
             <?php endif; ?>
           </div>
