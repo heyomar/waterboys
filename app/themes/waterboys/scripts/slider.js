b8dff8a3cee202ff62ff8acd80d4d3782df9ac65
Omar Rasool <orasool@hlkagency.com>
2016-05-13 17:00:02 -0500

    creates js modules for slider, adds table sorter js files, removes highlights page

---
+++ app/themes/waterboys/scripts/slider.js
@@ -0,0 +1,11 @@
+var $ = require('jquery');
+
+var slick = require('slick-carousel')
+
+module.exports = () => {
+  $(document).ready(function(){
+    $('#player-slider').slick({
+      setting-name: setting-value
+    });
+  });
+}
