11487d5e452a234ad5f25ab795acbde8ea71f400
Omar Rasool <orasool@hlkagency.com>
2016-05-13 17:01:31 -0500

    adds posts feed to index page witch will become the highlights page, also adds slick slider as a dependency  to package.json

--- app/themes/waterboys/index.php
+++ app/themes/waterboys/index.php
@@ -11,17 +11,33 @@
  * @since 0.1.0
  */
 
-get_header();
 
-  if ( have_posts() ) :
-    while ( have_posts() ) : the_post();
-      get_template_part('components/content', get_post_format());
-    endwhile;
-  else :
-    get_template_part('components/content', 'none');
-  endif;
 
-  posts_nav_link();
 
-get_sidebar();
-get_footer();
+
+ get_header(); ?>
+  <div class="wb__page-hero">
+    <h2 class="hl__title">Highlights<br>
+      <img class="hl__title-wave" src="<?php echo get_stylesheet_directory_uri() ?>/assets/wb__home-wave.png" alt="">
+    </h2>
+  </div>
+
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+<div class="hl__article-ctn">
+      <div class="mission-ctn hl__article-ctn">
+
+
+        <div class=" hl__flex-ctn">
+          <?php the_post_thumbnail('medium'); ?>
+          <div class="hl__flex-child-copy"><?php get_template_part('components/content', get_post_format()); ?></div>
+        </div>
+      </div>
+
+  </div>
+
+      <?php endwhile; ?>
+        <?php else:   get_template_part('components/content', 'none'); ?>
+          <?php endif ?>
+            <?php posts_nav_link(); ?>
+
+  <?php get_footer(); ?>
