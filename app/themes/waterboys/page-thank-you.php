6afbba9c582c7e70123a6ea2337739d9e87e4b33
Omar Rasool <orasool@hlkagency.com>
2016-05-19 13:13:00 -0500

    adds thank you page and css

--- app/themes/waterboys/page-thank-you.php
+++ app/themes/waterboys/page-thank-you.php
@@ -1,20 +1,22 @@
 <?php get_header(); ?>
   <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
     <div class="wb__page-hero">
-      <h2 class="st__title">Tackle Thirst<br><img class="st__title-wave" src="<?php echo get_stylesheet_directory_uri() ?>/assets/wb__home-wave.png" alt=""></h2>
+      <h2 class="st__title">Thank You<br><img class="st__title-wave" src="<?php echo get_stylesheet_directory_uri() ?>/assets/wb__home-wave.png" alt=""></h2>
     </div>
 
-    <div class="st__ctn">
-      <p>
-        Thank you for teaming up with us to tackle thirst. Your generosity will mean so much to the people we serve. Connect with us on social media to witness our work in real time and see what your contribution can accomplish.
-      </p>
-      <div class="ty__side-share-ctn">
-        <div class="socialbar-buttons-ctn">
-          <a href="" class="socialbar-button">
+    <div class="ty__ctn">
+      <div class="ty__inner-ctn">
+        <div class="ty__body-copy-ctn">
+          <p>Thank you for teaming up with us to tackle thirst. Your generosity will mean so much to the people we serve. Connect with us on social media to witness our work in real time and see what your contribution can accomplish.</p>
+        </div>
+
+        <div class="ty__side-share-ctn">
+          <a href="" class="socialbar-button ty__share-button">
             <svg class="icon icon-twitter">
               <use xlink:href="#icon-twitter"></use>
             </svg> Tweet</a>
-          <a href="" class="socialbar-button">
+          <br>
+          <a href="" class="socialbar-button ty__share-button">
             <svg class="icon icon-facebook-square">
               <use xlink:href="#icon-facebook-square"></use>
             </svg> Share</a>
@@ -22,10 +24,8 @@
       </div>
     </div>
 
-
-
     <div class="st__bar-ctn">
-      <p>14 million Tanzanians lack access to safe water. Dig deep with us. &nbsp;</p>
+      <p>Pile on: Every $1 spent on water and sanitation has a $4 return.&nbsp;</p>
     </div>
 
     <?php endwhile; ?>
