2ec70558a2db1d0cc01b13ec3c975e5775a868f9
Omar Rasool <orasool@hlkagency.com>
2016-05-24 17:06:03 -0500

    adds legal pages copy

--- app/themes/waterboys/page-photo-credits.php
+++ app/themes/waterboys/page-photo-credits.php
@@ -4,26 +4,41 @@
       <h2 class="st__title">Photo Credits<br><img class="st__title-wave" src="<?php echo get_stylesheet_directory_uri() ?>/assets/wb__home-wave.png" alt=""></h2>
     </div>
 
-   <div class="rs__ctn wb__page-legal">
-     <p>
-       Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-     </p>
-     <p>
-       Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-     </p>
-     <p>
-       Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-     </p>
-     <p>
-       Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-     </p>
-     <p>
-       Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-     </p>
-   </div>
-   <div class="st__bar-ctn">
-     <p>14 million Tanzanians lack access to safe water. Dig deep with us. &nbsp;</p>
-   </div>
-      <?php endwhile; ?>
+    <div class="rs__ctn wb__page-legal">
+      <p>
+        <a href="/">Waterboys Initiative</a> would like to thank the following for photos on our website. <br>
+        Photos courtesy of:
+      </p>
+      <ul>
+
+        <li>Arizona Cardinals Team Photography</li>
+        <li>Baltimore Ravens Team Photography </li>
+        <li>Buffalo Bills Team Photography </li>
+        <li>Carolina Panthers Team Photography </li>
+        <li>Bill Smith/Chicago Bears </li>
+        <li>Denver Broncos Team Photography </li>
+        <li>Getty Images</li>
+        <li>Houston Texans Team Photography</li>
+        <li>Indianpolis Colts Team Photography </li>
+        <li>Green Bay Packers Photography</li>
+        <li>Steve Sanders/Kansas City Chiefs </li>
+        <li>Miami Dolphins Team Photography </li>
+        <li>Minnesota Vikings Team Photography </li>
+        <li>New York Jets Team Photography </li>
+        <li>New England Patriots/Keith Nordstrom</li>
+        <li>Oakland Raiders</li>
+        <li>Philadelphia Eagles Team Photography </li>
+        <li>Pittsburgh Steelers Team Photography </li>
+        <li>St. Louis Rams Team Photography </li>
+        <li>Tennessee Titans</li>
+        <li>Nadus Films</li>
+        <li>Clay Cook Photography</li>
+
+      </ul>
+    </div>
+    <div class="st__bar-ctn">
+      <p>14 million Tanzanians lack access to safe water. Dig deep with us. &nbsp;</p>
+    </div>
+    <?php endwhile; ?>
       <?php endif ?>
-    <?php get_footer(); ?>
+        <?php get_footer(); ?>
--- app/themes/waterboys/page-privacy-policy.php
+++ app/themes/waterboys/page-privacy-policy.php
@@ -4,26 +4,56 @@
       <h2 class="st__title">Privacy Policy<br><img class="st__title-wave" src="<?php echo get_stylesheet_directory_uri() ?>/assets/wb__home-wave.png" alt=""></h2>
     </div>
 
-   <div class="rs__ctn wb__page-legal">
-     <p>
-       Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-     </p>
-     <p>
-       Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-     </p>
-     <p>
-       Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-     </p>
-     <p>
-       Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-     </p>
-     <p>
-       Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-     </p>
-   </div>
-   <div class="st__bar-ctn">
-     <p>14 million Tanzanians lack access to safe water. Dig deep with us. &nbsp;</p>
-   </div>
-      <?php endwhile; ?>
+    <div class="rs__ctn wb__page-legal">
+      <p>
+        This Privacy Policy is based on the federal Personal Information Protection and Electronic Documents Act ("PIPEDA"). It governs the relationship between Waterboys.Org ("Waterboys", "we", "us") and individuals with whom we interact through the Waterboys
+        website (the "Website"), such as our customer's rs and potential customers ("you"). As used herein, "Services" includes any version or type of the Waterboys services as well as downloads or other services provided by Waterboys.
+      </p>
+      <p>
+        Your right to privacy is very important.
+        <br> We recognize that when you choose to provide us with information about yourself, you trust us to act in a responsible manner. We believe this information should only be used to help us provide you with better service. That's why we have put this
+        Privacy Policy in place to protect your personal information. By your receipt of this Privacy Policy and your agreement to the Terms of Use, Waterboys will assume, unless we hear from you otherwise, that you consent to the collection, use and
+        disclosure of your personal information by Waterboys in accordance with this Privacy Policy.
+      </p>
+      <p>
+        What Personal Information Do We Collect?
+        <br> In general, when you visit our Website and access information you remain anonymous. We will not provide any of your personal information to other companies or individuals without your permission. We collect only that information that we need
+        in order to maintain a high level of customer service to you; to manage, develop and improve our operations; and to understand your needs. Sometimes that is only a name and an email address, but in some circumstances, we need to have more information.
+        We will limit our collection, use and disclosure of your personal information to the purposes that we have identified to you. The only personally identifiable information we collect and store is the information you choose to provide us with either
+        when you register or order a download on the Website. In addition, we may occasionally collect information in the course of a contest or promotion. At the time that we collect this information we will give you the opportunity to opt out of receiving
+        further offers and information from Waterboys.
+      </p>
+      <p>
+        Protecting your Privacy
+        <br> We will take appropriate steps to protect your privacy. Whenever you provide personal information, we will take reasonable steps to protect it. We will also take reasonable security measures to protect your personal information in storage. As
+        mentioned above, we will not provide any of your personal information to other companies or individuals without your permission. We do not sell names, email addresses, mailing addresses, or other information to third parties for any purpose. We
+        will only disclose the information when required by law or for the purpose of a proposed sale of all of or a part of our business, in which case, we will obtain the agreement of third parties to protect the confidentiality of your information.
+
+      </p>
+      <p>
+        Your Credit Card Information
+        <br> Please note that when making payment for content form the Website, you will be providing your information on method of payment and credit card details to PayPal Inc., a payment processing company certified by Verisign that has a contract with
+        Waterboys. All details of your method of payment and credit card details are UNAVAILABLE to Waterboys and they never come into Waterboys's knowledge or possession. Nevertheless, Waterboys agrees to keep confidential and protect any other financial
+        information on its users with all possible care.
+      </p>
+      <p>
+        Use of Cookies
+        <br> Waterboys may occasionally use cookies to provide you with a better experience on the Website. A cookie is useful information, such as an e mail address or password, which is stored on your hard drive that enables your web browser to "remember"
+        some useful items when you return to a web site. Cookies from the Website can only be read by the Website. If you choose to disable cookies in your browser, you will still be able to access the Website, although your user experience may be diminished.
+        The Website may collect visitor information in the form of the visitor's domain or IP address solely for statistical purposes and to ensure that copyright territory restrictions are respected, and may also collect information regarding which pages
+        are accessed. This information is used internally, only in aggregate form, to better serve you by helping us manage and improve the content of our Website and to diagnose technical problems.
+
+      </p>
+      <p>
+        Questions?
+        <br> Waterboys welcomes comments and questions on this Privacy Policy. We are dedicated to protecting your personal information, and will make every reasonable effort to keep that information secure. We may occasionally update this Privacy Policy;
+        all revisions will be posted to this site. To access your personal information, change your consent to the ways in which we may use your personal information, have a complaint about our Privacy Policy or the manner in which we may have handled
+        your personal information, or if you have any questions about our Privacy Policy, please contact our Privacy Officer (who is responsible for this Privacy Policy and accountable to you) as follows: Privacy Officer Email: privacy.office@Waterboys.org
+      </p>
+    </div>
+    <div class="st__bar-ctn">
+      <p>14 million Tanzanians lack access to safe water. Dig deep with us. &nbsp;</p>
+    </div>
+    <?php endwhile; ?>
       <?php endif ?>
-    <?php get_footer(); ?>
+        <?php get_footer(); ?>
