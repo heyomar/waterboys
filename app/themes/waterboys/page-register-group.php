0bb37922e1805951f6d7d416db8bbcefea9d830e
Omar Rasool <orasool@hlkagency.com>
2016-08-10 08:55:38 -0500

    adds new hero for register a new group page

--- app/themes/waterboys/page-register-group.php
+++ app/themes/waterboys/page-register-group.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
   <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-    <div class="wb__page-hero">
+    <div class="wb__page-hero gp__page-hero">
       <h2 class="st__title">Register A New Group<br><img class="st__title-wave" src="<?php echo get_stylesheet_directory_uri() ?>/assets/wb__home-wave.png" alt=""></h2>
     </div>
 
