0c53005fbb361daf1356266e427fe866a688ff1e
Omar Rasool <orasool@hlkagency.com>
2016-08-12 10:19:42 -0500

    adds home video bg

--- app/themes/waterboys/front-page.php
+++ app/themes/waterboys/front-page.php
@@ -1,11 +1,17 @@
 <?php get_header(); ?>
 
   <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+
     <div class="wb__home-container">
+      <video id="home__video-bg" autoplay="true" loop muted="true">
+        <source src="<?php echo get_stylesheet_directory_uri(); ?>/assets/video/home__video-bg.mp4" type="video/mp4">
+      </video>
+
       <div class="wb__home-content">
         <h1 class="wb__hero-title">
 		        Team Up to <br> Tackle Thirst
-		    <h2>
+		    </h1>
 		    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/wb__home-wave.png" alt="" />
 		    <h3 class="wb__hero-subtitle">
 		        Waterboys unites players and fans from across the NFL in support of one mission:<br>bringing life-sustaining well water to East African communities in need.
