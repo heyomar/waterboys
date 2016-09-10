e6481b0609dec87745111d678c58f1ba93ae40c7
Paul Graham <pgraham@hlkagency.com>
2016-09-10 00:35:52 -0500

    fix header style on thank you

--- app/themes/waterboys/page-thank-you.php
+++ app/themes/waterboys/page-thank-you.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
   <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-    <div class="wb__page-hero" style="background: url('<?php echo get_stylesheet_directory_uri(); ?>/assets/header__donate.jpg') top center no-repeat;">
+    <div class="wb__page-hero" style="background: url('<?php echo get_stylesheet_directory_uri(); ?>/assets/header__donate.jpg') top center no-repeat; background-size: cover;">
       <h2 class="st__title">Thank You<br><img class="st__title-wave" src="<?php echo get_stylesheet_directory_uri() ?>/assets/wb__home-wave.png" alt=""></h2>
     </div>
 
