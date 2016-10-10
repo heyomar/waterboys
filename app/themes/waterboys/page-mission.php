a03ba8db289715485d3fcdf656e04c6a1ddf1dfd
Omar Rasool <orasool@hlkagency.com>
2016-10-10 10:24:31 -0500

    fixes CTA on mission page

--- app/themes/waterboys/page-mission.php
+++ app/themes/waterboys/page-mission.php
@@ -126,14 +126,15 @@
     </div>
 
 
-    <div class="wb__mission-socialbar-ctn">
-      <p class="socialbar-copy">Bring it in. Connect with us on social.</p>
-      <div class="socialbar-buttons-ctn">
-        <a href="https://twitter.com/waterboysorg" target="_blank" class="socialbar-button wb__button">
+    <div class="cta">
+      <p>Bring it in. Connect with us on social.</p>
+      <div class="share">
+        <a target="_blank" href="http://twitter.com/intent/tweet?text=I+just+teamed+up+to+tackle+thirst+with+@WaterboysORG.+Help+your+favorite+%23NFL+team+dig+deep+and+change+lives:" target="_blank" class="socialbar-button wb__button">
           <svg class="icon icon-twitter">
             <use xlink:href="#icon-twitter"></use>
           </svg> Tweet</a>
-        <a href="https://www.facebook.com/waterboysorg" target="_blank" class="socialbar-button wb__button">
+          <span class="space"></span>
+        <a target="_blank" href="https://www.facebook.com/waterboysorg" target="_blank" class="socialbar-button wb__button">
           <svg class="icon icon-facebook-square">
             <use xlink:href="#icon-facebook-square"></use>
           </svg> Share</a>
