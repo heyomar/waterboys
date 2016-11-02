7ce6f0b594c4d61f59ba0ba25edec7f424bfc1c7
Omar Rasool <orasool@hlkagency.com>
2016-11-02 12:02:34 -0500

    adds 404 page

--- app/themes/waterboys/404.php
+++ app/themes/waterboys/404.php
@@ -9,17 +9,34 @@
  * @license http://opensource.org/licenses/MIT
  * @since 0.1.0
  */
+?>
 
-get_header(); ?>
+<?php get_header(); ?>
+<div class="wb__page-hero legal__hero-bg">
+	<h2 class="st__title">Fumble<br>
+	<img class="st__title-wave" src="<?php echo get_stylesheet_directory_uri() ?>/assets/wb__home-wave.png" alt="">
+	</h2>
+</div>
 
-  <header class="article__header">
-    <h1 class="article__title"><?php _e('404 Not Found', 'offset'); ?></h1>
-  </header>
+<div class="error404page">
+	<h3>This page doesn't exist. Recover <a href="/">here</a >.</h3>
+	<p class="copy">404 Error: Page Not Found</p>
+</div>
 
-  <div class="article__content">
-    <p><?php _e('Nothing was found at this URL. Try searching for your page below.', 'offset'); ?></p>
-  </div><?php
+		<div class="cta">
+			<p>Bring it in. Connect with us on social.</p>
+			<div class="share">
+				<a target="_blank" href="http://twitter.com/intent/tweet?text=I+just+teamed+up+to+tackle+thirst+with+@WaterboysORG.+Help+your+favorite+%23NFL+team+dig+deep+and+change+lives:" target="_blank" class="socialbar-button wb__button">
+					<svg class="icon icon-twitter">
+						<use xlink:href="#icon-twitter"></use>
+					</svg> Tweet</a>
+					<span class="space"></span>
+				<a target="_blank" href="https://www.facebook.com/waterboysorg" target="_blank" class="socialbar-button wb__button">
+					<svg class="icon icon-facebook-square">
+						<use xlink:href="#icon-facebook-square"></use>
+					</svg> Share</a>
+			</div>
+		</div>
 
-  get_search_form();
 
-get_footer();
+<?php get_footer(); ?>
