28b4ca981465df8dceac81c3df40535919f4b763
Omar Rasool <orasool@hlkagency.com>
2016-11-14 13:31:05 -0600

    updates group template

--- app/themes/waterboys/single-group.php
+++ app/themes/waterboys/single-group.php
@@ -8,8 +8,7 @@ Template Name: Group Profile
         <div class="gp__page-hero-ctn">
               <div class="gp__page-hero-inner-ctn">
                 <div class="gp__flex-profile-image">
-                  <?php if (get_field('profile_image')): ?><img class="gp__profile-image" src="<?php the_field('profile_image'); ?>" />
-                  <?php endif; ?>
+                  <img class="gp__profile-image" src="<?php the_post_thumbnail_url(); ?>" />
                 </div>
                 <div class="gp__wave-ctn">
                   <img class="st__title-wave" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/wb__home-wave.png" alt="">
@@ -36,7 +35,7 @@ Template Name: Group Profile
           ?>
           <div class="wb__page-dash-item">
             <h3>Fundraising Goal</h3>
-            <div class="dash__goal"><?php echo $donationTotal; ?></div>
+            <div class="dash__goal"><?php echo the_field('register_group_fundraising_goal') ?></div>
           </div>
           <div class="wb__page-dash-item">
             <h3>Progress to Goal</h3>
@@ -44,9 +43,7 @@ Template Name: Group Profile
               <div class="wb__progressgoal-bar"></div>
             </div>
             <p class="wb__progressgoal-amount">
-                <?php echo
-                $donationTotal 
-                ?>
+                <?php echo $donationTotal ?>
             </p>
           </div>
           <?php $player = get_field('affiliated_player'); ?>
@@ -74,21 +71,22 @@ Template Name: Group Profile
               Location
             </p>
             <p class="wb__player__content__body">
-              <?php the_field('register_group_city'); ?>,<?php the_field('register_group_state'); ?>
+              <?php the_field('register_group_city'); ?>,&nbsp;<?php the_field('register_group_state'); ?>
             </p>
             <p class="wb__player__content__title wb__player__bio-title">
               Group Contact
             </p>
             <p class="wb__player__content__body">
-              <?php the_field('group_contact_first_name'); ?><?php the_field('group_contact_last_name');  ?>
+              <?php the_field('group_contact_first_name'); ?>&nbsp;<?php the_field('group_contact_last_name');  ?>
             </p>
           </div>
         </div>
+
         <div class="wb__player-content wb__content-info">
           <p class="wb__player__content__info-position wb__player__content__title wb__player__info-title">
             About Waterboys
           </p>
-          <p>
+          <p class="sidebar-body">
             Waterboys unites players and fans from across the NFL to bring life-sustaining well water to East African communities in need. We provide clean, safe and sustainable water access by hiring local crews to drill deep-bore wells serving up to 7,500 people. At a cost of $45,000 per well, every drop counts.
           </p>
 
