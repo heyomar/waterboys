883413d6693c0ffecabb1efe02dd6da9680eafb1
Omar Rasool <orasool@hlkagency.com>
2016-12-13 15:00:20 -0600

    adds table sorter

--- app/themes/waterboys/header.php
+++ app/themes/waterboys/header.php
@@ -22,7 +22,7 @@
     <script src="https://use.typekit.net/wmb3pty.js"></script>
     <script>try{Typekit.load({ async: true });}catch(e){}</script>
 
-    <script>
+		<script>
     (function(i,s,o,g,r,a,m){i[‘GoogleAnalyticsObject’]=r;i[r]=i[r]||function(){
     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -31,7 +31,6 @@
     ga('send’, 'pageview’);
     </script>
   </head>
-
   <body <?php body_class(); ?>>
     <?php get_template_part('components/svg') ?>
     <header role="banner" class="site__header">
